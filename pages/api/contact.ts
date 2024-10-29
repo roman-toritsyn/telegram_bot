655812385adf0f164d595ddf8b25099e93009e56
Roman Toritsyn <roman.toritsyn@otakoyi.com>
2024-10-29 16:32:29 +0200

    delete_form

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   if (req.method === "POST") {
     const { message } = req.body;
 
